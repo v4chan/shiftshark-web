e12666ef599415cb1264a8f9df68ff43447d03d8
koala-kalda <kalev.a.kalda@gmail.com>
2016-10-02 16:37:38 -0400

    fixed some deployment issues

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,26 +1,30 @@
+
 require 'dotenv'
 Dotenv.load
+rails_env = ENV['RAILS_ENV']
 
 # set path to application
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
+app_dir = File.expand_path( "../..", __FILE__)
+shared_dir = "#{app_dir}/run"
 working_directory app_dir
 port = ENV.fetch("RAILS_PORT", 3000 )
 
 # Set unicorn options
-worker_processes ENV.fetch("RAILS_WORKERS", 2 ).to_i
+worker_processes ENV.fetch( "RAILS_WORKERS", 2 ).to_i
 preload_app true
 timeout 30
 
 # Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
-if ENV['RAILS_ENV'] != 'production'
+listen "#{shared_dir}/unicorn.sock", :backlog => 64
+if rails_env != 'production'
 	listen "localhost:#{port}"
 end
 
 # Logging
-stderr_path "#{shared_dir}/log/unicorn.err.log"
-stdout_path "#{shared_dir}/log/unicorn.out.log"
+if [ 'beta', 'production'].include? rails_env
+	stderr_path "#{shared_dir}/unicorn.err.log"
+	stdout_path "#{shared_dir}/unicorn.out.log"
+end
 
 # Set master PID location
-pid "#{shared_dir}/pids/unicorn.pid"
+pid "#{shared_dir}/unicorn.pid"
