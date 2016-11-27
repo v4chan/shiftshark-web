8c59e69290a8e42b46716e6a5eee491e98576e70
koala-kalda <kalev.a.kalda@gmail.com>
2016-11-27 16:19:22 -0500

    worked out some css issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+	protect_from_forgery with: :exception
 
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+	before_filter :configure_permitted_parameters, if: :devise_controller?
 
-    protected
-
-        def configure_permitted_parameters
-            
-            devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :addr1, :addr2, :phone, :email, :bnum, :base_rate, :closing) }
-        end
+	protected def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:account_update) {
+			|u| u.permit(
+				:name, :email, :password, :current_password,
+				:addr1, :addr2, :phone, :email, :bnum, :base_rate, :closing) }
+	end
 end
